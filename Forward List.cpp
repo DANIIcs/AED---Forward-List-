fd1d5563be785bfede03f7f009e3a9f53024c042
Daniel <humbertodanielcaballeros@gmail.com>
2024-08-28 12:09:49 -0500

    a

--- Forward List.cpp
+++ Forward List.cpp
@@ -2,18 +2,51 @@
 #include <forward_list>
 using namespace std;
 
-T front();           // Retorna el elemento al comienzo
-T back();            // Retorna el elemento al final
-void push_front(T);  // Agrega un elemento al comienzo
-void push_back(T);   // Agrega un elemento al final
-T pop_front();       // Remueve el elemento al comienzo
-T pop_back();        // Remueve el elemento al final
-T operator[](int);   // Retorna el elemento en la posición indicada
-bool empty();        // Retorna si la lista está vacía o no
-int size();          // Retorna el tamaño de la lista
-void clear();        // Elimina todos los elementos de la lista
-void sort();         // Implementa un algoritmo de ordenación con listas enlazadas
-void reverse();      // Revierte la lista
+// Retorna el elemento al comienzo
+T front();      
+
+// Retorna el elemento al final
+T back();       
+
+// Agrega un elemento al comienzo
+void push_front(T);  
+
+// Agrega un elemento al final
+void push_back(T);  
+
+// Remueve el elemento al comienzo
+T pop_front();       
+
+// Remueve el elemento al final
+T pop_back();
+
+// Retorna el elemento en la posición indicada
+T operator[](int);
+
+// Retorna si la lista está vacía o no
+bool empty();
+
+// Retorna el tamaño de la lista
+int size();
+
+// Elimina todos los elementos de la lista
+void clear() {      
+    while (head != nullptr) {
+        Nodo* temp = head;
+        head = head->next;
+        delete temp;
+    }
+    head = nullptr;
+}
+
+// Implementa un algoritmo de ordenación con listas enlazadas
+void sort(); {
+
+}
+
+// Revierte la lista
+void reverse();      
+
 
 
 
